7ee777fab207650a9fd537856c8236f4808df33f
Murat <murathpdev@outlook.com>
2025-09-13 08:25:24 -0400

    Initial commit - Hello World Flask app

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template_string
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return """
+    <h1>Hello, World!</h1>
+    <form>
+      <label>Name:</label>
+      <input type="text" name="name"><br><br>
+      <label>
+        <input type="checkbox" name="subscribe"> Subscribe to newsletter
+      </label><br><br>
+      <button type="submit">Submit</button>
+    </form>
+    """
+
+if __name__ == "__main__":
+    app.run(debug=True)
